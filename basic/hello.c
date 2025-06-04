db8f9fb991eac7cfb638f6f508ca0662cf1ca835
Ren_Haiyang <ocean_97@163.com>
2025-06-05 00:51:21 +0800

    <test>(no):a simple test for C language
    Signed-off-by: Ren_Haiyang <ocean_97@163.com>

---
+++ basic/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello world!\n");
+    exit(0);
+}
+
+/*
+1. what is the diffrent between exit(0) and return 0;
+2. what is the diffrent between main() and main(void);
+3. which cmd can compile the program into the specified exe file;
+4. What is the compilation process of the C language;
+*/
